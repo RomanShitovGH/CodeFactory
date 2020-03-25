8ec9789c5629871ae9f1ea9561321a5cfc8d3c28
RomanShitovGH <romanshitov@yandex.ru>
2020-03-25 09:58:48 +0300

    готовое 7.7

--- static/pages/IndexPage.jsx
+++ static/pages/IndexPage.jsx
@@ -9,44 +9,41 @@ export default class IndexPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      products: {}  
+      products: []  
     }
   }
   
-  componentDidMount(){
+  componentDidMount() {
     console.log("Функция componentDidMount() начала выполняться");
     fetch("/api/products")
-      .then(function(response) {
+      .then(function (response) {
         return response.json();
       })
-      .then(function(json) {
+      .then(function (json) {
         this.setState({
-          products: json 
-        })
-        console.log(this.state.products);
-        ;
-      }.bind(this))
+          products: json
+        });
+      }.bind(this)
+      )
+      
   }
   
-   renderProducts() { 
-      //console.log("зашло в renderProducts()");
-      //if (!this.state.products) { return false; } 
-      //console.log(this.state.products);
-      this.state.products.map(product => {
-        return (
-          <div className="card">
-            бла бла бла
-            <img className="card-img-top" src="https://www.codery.school/content/course/lesson3-task-img.png" alt="Card image cap"/> 
-            <div className="card-body">
-              <h5 className="card-title">{product.title}</h5>
-              <p className="card-text">Описание первого товара</p>
-              <p className="card-text">Цена: 1000</p>
-              <Link to="/product/12345-slug">Купить</Link>          
-            </div>
+  renderProducts() {
+    return this.state.products.map(product => {
+      return (
+        <div className="card">
+          <img className="card-img-top" src={product.img} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{product.title}</h5>
+            <p className="card-text">{product.description}</p>
+            <p className="card-text">Цена: {product.price}</p>
+            <Link to="/product/12345-slug">Купить</Link>
           </div>
-        );
-      }) 
-  } 
+        </div>
+      );
+    })
+  }
+   
    
       
   render() {
@@ -66,12 +63,11 @@ export default class IndexPage extends React.Component {
                   <div className="row">
                     <div className="col-md-8 offset-md-2 col-sm-10 offset-sm-1 fon">
                       <div className="card-deck">
-                      { 
-                          this.state.products
-                            ? this.renderProducts() 
+                      { this.state.products
+                          ? this.renderProducts() 
                             : false
-                        }
-                        <div className="card">
+                      }
+                        {/* <div className="card">
                           <img className="card-img-top" src="https://www.codery.school/content/course/lesson3-task-img.png" alt="Card image cap"/> 
                           <div className="card-body">
                             <h5 className="card-title">Первый товар</h5>
@@ -97,7 +93,7 @@ export default class IndexPage extends React.Component {
                             <p className="card-text">Цена: 3000</p>
                             <Link to="/product/22222-slug">Купить</Link>          
                           </div>
-                        </div>
+                        </div> */}
                       </div>      
                     </div>
                   </div>
